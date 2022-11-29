66fbf39326cec1a5cb6131a5de289b642cd264a1
jahir509 <me.jahirahmed@gmail.com>
2022-11-29 13:08:56 +0600

    Update: Api Endpoint for Activities

--- API/Controllers/Base/BaseApiController.cs
+++ API/Controllers/Base/BaseApiController.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace API.Controllers.Base
 {
@@ -12,6 +7,8 @@ namespace API.Controllers.Base
     [Route("api/[controller]")]
     public class BaseApiController : ControllerBase
     {
-       
+       private IMediator _mediator;
+
+       protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
     }
 }
