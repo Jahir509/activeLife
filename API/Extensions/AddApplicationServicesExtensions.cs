e6318129621198378fa05ddf1444ba4180122181
jahir509 <me.jahirahmed@gmail.com>
2022-11-29 13:07:09 +0600

    Update: Cleanup Startup and extend services to A Method

---
+++ API/Extensions/AddApplicationServicesExtensions.cs
@@ -0,0 +1,36 @@
+using Application.Activities;
+using Application.core;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Persistence;
+
+namespace API.Extensions
+{
+    public static class AddApplicationServicesExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services,IConfiguration _config)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
+            });
+
+            services.AddDbContext<DataContext>(option=>{
+                option.UseSqlite(_config.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddCors(option=>{
+                option.AddPolicy("CorsPolicy",policy=>{
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000"); 
+                });
+            });
+
+            services.AddMediatR(typeof(List.Handler).Assembly);
+            services.AddAutoMapper(typeof(MappingProfile).Assembly);
+
+
+            return services;
+        }
+    }
+}
