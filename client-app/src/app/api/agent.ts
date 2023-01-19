f8aab830f8f70e5adbebe86bc394919dadf33247
jahir509 <me.jahirahmed@gmail.com>
2023-01-19 16:14:30 +0600

    Add: env Properties

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -15,7 +15,7 @@ const sleep = (delay: number)=> {
 }
 
 
-axios.defaults.baseURL = 'http://localhost:5000/api';
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
 const responseBody = <T> (response: AxiosResponse<T>)=> response.data;
 
@@ -30,7 +30,7 @@ axios.interceptors.request.use(config => {
 
 // Response Interceptor
 axios.interceptors.response.use(async response => {
-        await sleep(1000);
+        if(process.env.NODE_ENV === 'development') await sleep(1000);
         const pagination = response.headers['pagination'];
         if(pagination){
             response.data = new PaginatedResult(response.data,JSON.parse(pagination))
